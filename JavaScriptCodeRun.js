54fe39e1792c7cafff9b19509a3e11643fd7f945
Ana Carrillo <acarrillo9724@gmail.com>
2020-07-24 14:48:17 -0400

    variable types

--- JavaScriptCodeRun.js
+++ JavaScriptCodeRun.js
@@ -1,6 +1,23 @@
-var i = 5;
-// gdgshkjsdhgl
-/*dgsdgsdgs
-sgdgfgsd
-sdgdg*/
-console.log(i)
+/* variables types*/
+//var are flexible and can be changed
+// numberical type variables
+var age = 23;
+var money = 50.05;
+age = 28;
+
+// bolleans
+var isUserLoggedIn = true;
+
+// strings
+
+var name = "James";
+name = 'Peter';
+
+name = 60 // var variables you can change type and value (string -> number)
+
+
+// constants - better used for variables that are not changing
+const title = "My webpage"; //type cannot change once assigned
+//title "ajhfa"; this would couse an error 
+
+console.log(title)
