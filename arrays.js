ef0cc6c8bdb2566bc8d16fc7cc892f77f49d1984
Ana Carrillo <acarrillo9724@gmail.com>
2020-07-25 18:45:30 -0400

    arras review

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var shoppingList = ["butter", "milk", "eggs"];
+console.log(shoppingList);
+
+shoppingList = ["Eggs", "Butter", "Apples"]; // you can replace data in an array
+var eggs = shoppingList[0]; // you can assign new data to an index
+shoppingList[1] = "Cheese"; // same here
+
+var listLenght = shoppingList.length()
+
+shoppingList.push("butter");
+shoppingList.pop();
