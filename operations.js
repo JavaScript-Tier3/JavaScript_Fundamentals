0e6ef4145666f1d67a55a640bb2f4dd1c20cadc4
Ana Carrillo <acarrillo9724@gmail.com>
2020-07-24 20:21:20 -0400

    operations

---
+++ operations.js
@@ -0,0 +1,15 @@
+// Assignment: =
+var age = 32;
+var name = "Ana"
+var money = 5000
+// Arithmetic: + - * / %
+age = age + 1;
+var newAge = age + 4;
+age += 1;
+var fullName = name + "Carrillo";
+// Comparison: == !=  > >= < <= this return either true or false
+var canAfford = money >= 20;
+// Logical: || && !
+var cannotAfford = !canAfford; // this gives the opposite so it will evaluate to false
+var canBuy = canAfford == True && age >= 18;
+var mightBuy = canAfford == True || age >= 18;
