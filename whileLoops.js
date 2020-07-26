677a4e729bc72883f9ae8ef28c37e600ddc174dd
Ana Carrillo <acarrillo9724@gmail.com>
2020-07-25 20:41:07 -0400

    while loops review

---
+++ whileLoops.js
@@ -0,0 +1,10 @@
+// while loops allow us to run code or set of statements multiple times without having to type them each time
+var pos = 0;
+const endPos = 5;
+
+while (pos < endPos) {
+    pos += 1;
+     console.log(pos);
+}
+
+// goes 1 and 2 and 3 and 4 and until it reaches 5 and then skips it and exits the loop
